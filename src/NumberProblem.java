4bf78a08699cf240922f7644438f6dcc63bf8fe3
dylaneckersall <dylan.eckersall97@outlook.com>
2017-02-05 03:08:22 +0000

    Added the NumberProblem abstract class

---
+++ src/NumberProblem.java
@@ -0,0 +1,14 @@
+
+/**
+ * Number problem abstract class declares common methods
+ * which all NumberProblems must implement. 
+ * @author Dylan
+ *
+ */
+public abstract class NumberProblem {
+	
+	public abstract void easyNumberProblem();
+	public abstract void mediumNumberProblem();
+	public abstract void hardNumberProblem();
+	
+}
